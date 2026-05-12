21fad6a855b5351cd3c03a34a328e1bc83b376c2
Tat Uyen Tam <tattam192@gmail.com>
2026-05-12 16:47:03 +0700

    feat(packing): setup layout and utilities for packing feature

--- src/features/packing/packingSlice.js
+++ src/features/packing/packingSlice.js
@@ -1,22 +1,36 @@
+import { loadData, saveData, TRIP_PROPERTIES } from "@/services/tripDataService";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  count: 0
+  checklist: loadData(TRIP_PROPERTIES.PACKING_LIST),
 };
 
 export const packingSlice = createSlice({
   name: "packing",
   initialState,
   reducers: {
-    increment: (state) => {
-      state.count += 1;
+    addToChecklist: (state, action) => {
+      state.checklist.push(action.payload);
+      saveData(TRIP_PROPERTIES.PACKING_LIST, state.checklist);
     },
-    decrement: (state) => {
-      state.count -= 1;
+    updateCheckList: (state, action) => {
+      const itemIdx = state.checklist.findIndex((item) => item.id === action.payload.id);
+
+      if (itemIdx !== -1) {
+        state.checklist[itemIdx] = {
+          ...state.checklist[itemIdx],
+          ...action.payload
+        }
+      }
+      saveData(TRIP_PROPERTIES.PACKING_LIST, state.checklist);
+    },
+    removeFromCheckList: (state, action) => {
+      state.checklist = state.checklist.filter((item) => item.id !== action.payload);
+      saveData(TRIP_PROPERTIES.PACKING_LIST, state.checklist);
     }
   },
 });
 
-export const { increment, decrement } = packingSlice.actions;
+export const { addToChecklist, updateCheckList, removeFromCheckList } = packingSlice.actions;
 
 export default packingSlice.reducer;
