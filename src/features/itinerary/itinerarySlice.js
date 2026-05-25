7ee0b9fd7708cef847b1db2e233d6aafe250762b
Tat Uyen Tam <tattam192@gmail.com>
2026-05-25 10:59:07 +0700

    refactor: remove unnecessary filter in itinerary slice

--- src/features/itinerary/itinerarySlice.js
+++ src/features/itinerary/itinerarySlice.js
@@ -21,13 +21,15 @@ export const itinerarySlice = createSlice({
       })
 
       // Clear itinerary
-      .addCase(clearTripData.fulfilled, (state, action) => {
-        state.items = state.items.filter(
-          (item) => item.tripId !== action.payload.tripId,
-        );
+      .addCase(clearTripData.fulfilled, (state) => {
+        // state.items = state.items.filter(
+        //   (item) => item.tripId !== action.payload.tripId,
+        // );
+        state.items = [];
       })
-      .addCase(deleteTrip.fulfilled, (state, action) => {
-        state.items = state.items.filter((item) => item.tripId !== action.payload);
+      .addCase(deleteTrip.fulfilled, (state) => {
+        // state.items = state.items.filter((item) => item.tripId !== action.payload);
+        state.items = [];
       })
       .addCase(logout, (state) => {
         state.items = [];
